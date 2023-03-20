ed7b17e3381010bba11c7e8c069215510a4953e2
gosuuk <tjddnr633@naver.com>
2023-03-20 22:01:19 +0900

    3월 20일(2)

---
+++ src/ch10/ContantTest.java
@@ -0,0 +1,13 @@
+package src.ch10;
+
+public class ContantTest {
+    public static void main(String[] args) {
+        final int Max_NUM = 100;
+        final int MIN_NUM;
+        MIN_NUM = 0;
+        // Max_NUM = 20;
+
+        System.out.println(Max_NUM);
+        System.out.println(MIN_NUM);
+    }
+}
