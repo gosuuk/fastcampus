d7db5e751bac4bea804a0012b3fe092319d5803b
ParkSeongUk <tjddnr633@naver.com>
2023-03-24 15:47:57 +0900

    3월24일(1)

---
+++ src/chat2/ch14/TakeTransTest.java
@@ -0,0 +1,26 @@
+package src.chat2.ch14;
+
+public class TakeTransTest {
+
+    public static void main(String[] args) {
+        Student studentP = new Student("Pakr", 5000);
+        Student studentK = new Student("kim", 10000);
+
+        Bus bus100 = new Bus(100);
+
+        Subway subwayGreen = new Subway(2);
+
+
+        studentP.takeBus(bus100);
+        studentK.takeSubway(subwayGreen);
+
+        studentP.showInfo();
+        studentK.showInfo();
+
+        bus100.showBusInfo();
+
+        subwayGreen.showSubwayInfo();
+    }
+
+}
+
